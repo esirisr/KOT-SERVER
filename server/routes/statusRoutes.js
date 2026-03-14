6666d8a4fd33de2c8c133594a229c556ba4b72f8
esirisr <esirisr123@gmail.com>
2026-03-14 11:40:11 +0300

    tests

--- server/routes/statusRoutes.js
+++ server/routes/statusRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getStatus, updatePaymentStatus, getAnalytics } from '../controllers/statusController.js';
+import { getStatus, updatePaymentStatus, getAnalytics, checkStoreUnique } from '../controllers/statusController.js';
 
 const router = express.Router();
 
-router.get('/analytics', getAnalytics); // New Analytics Route
+router.get('/analytics', getAnalytics);
+router.get('/check-store', checkStoreUnique); // New endpoint for store validation
 router.get('/:phone', getStatus);
-router.put('/:phone', updatePaymentStatus); // New Admin Update Route
+router.put('/:phone', updatePaymentStatus);
 
 export default router;
